e822fd717bb96b7f9b4b7387e12cb058d54c521f
NeocortexF <eugeneshamkin@mail.ru>
2017-12-26 10:12:49 +0500

    update of parsing decision before applying date system configuration

--- grails-app/controllers/phonebook/PhonebookController.groovy
+++ grails-app/controllers/phonebook/PhonebookController.groovy
@@ -88,31 +88,93 @@ class PhonebookController {
     }
 
     def upload = {
-        request.getFile('filecsv')
-                .inputStream
-                .splitEachLine(';') { fields ->
-            def phonebook = new Phonebook(name: fields[1].trim(),
+
+        String birthdayField
+
+        //!! вот оно
+        request.getFile('filecsv').inputStream.splitEachLine(';') { line ->
+            if(line.contains('#')){
+            } else {
+
+                birthdayField = line[4] //!!
+
+                def phonebook = new Phonebook(name: fields[1].trim(),
                     surname: fields[2].trim(), patronymic: fields[3].trim(),
-                    telephone: fields[5].trim(),
+                        birthday: birthdayField, telephone: fields[5].trim(),
                     eMail: fields[6].trim())
 
-            //birthday: fields[4].trim(),
-
-            if (phonebook.hasErrors() || phonebook.save(flush: true) == null) {
+                if (phonebook.hasErrors() || phonebook.save(flush: true) == null) {
                 log.error("Could not import domainObject  ${phonebook.errors}")
             }
 
             log.debug("Importing domainObject  ${phonebook.toString()}")
+
+            }
         }
 
-//        def stringList = phonebookService.list()
-//        String result = stringList.join(",")
-//        result.each {
-//            println "${it.each {print({it}.toString())}}"
+//        def list = request.getFile('filecsv').inputStream.splitEachLine(';').collect {it}
+//        println(list)
+
+
+//        def file = request.getFile('filecsv')
+//        StringBuilder sb = new StringBuilder()
+//        def fs = file.getInputStream()
+//
+//        int ch
+//        while((ch = fs.read()) != -1){
+//            sb.append((char)ch)
+//        }
+//
+//        String fileFullContent = sb.toString()
+//        String[] wordArray = fileFullContent.split('$')
+//
+//
+//        println(wordArray.toString())
+//        println(wordArray.length)
+
+//        println(wordArray[0])
+//        println(wordArray[6])
+//        println(wordArray[6])
+
+
+
+//        for(int i = 0; i <= wordArray.length; i=i+7){
+//            idField = wordArray[i]
+//            nameField = wordArray[i+1]
+//            surnameField = wordArray[i+2]
+//            patronymicField = wordArray[i+3]
+//            birthdayField = wordArray[i+4]
+//            telephoneField = wordArray[i+5]
+//            eMailField = wordArray[i+6]
+//
+//            println("\nСледущая сторока " + idField +" "+ nameField +" "
+//            + surnameField +" " + patronymicField +" " + birthdayField +" " + telephoneField +" "+ eMailField)
+//
 //        }
 
-
     }
+
+
+
+
+//        request.getFile('filecsv')
+//                .inputStream
+//                .splitEachLine(';') { fields ->
+//            def phonebook = new Phonebook(name: fields[1].trim(),
+//                    surname: fields[2].trim(), patronymic: fields[3].trim(),
+//                    telephone: fields[5].trim(),
+//                    eMail: fields[6].trim())
+
+//birthday: fields[4].trim().toDate(),
+//            if (phonebook.hasErrors() || phonebook.save(flush: true) == null) {
+//                log.error("Could not import domainObject  ${phonebook.errors}")
+//            }
+//
+//            log.debug("Importing domainObject  ${phonebook.toString()}")
+//        }
+
+//
+//    }
 
     protected void notFound() {
         request.withFormat {
--- grails-app/views/phonebook/index.gsp
+++ grails-app/views/phonebook/index.gsp
@@ -27,46 +27,40 @@
         <div class="panel-body col-lg-12 col-md-12">
             <div class="table-responsive col-lg-6 col-md-6 col-sm-12 col-xs-12">
                 <h1 class="h3"><g:message code="default.list.phonebook" args="[entityName]"/></h1>
-                <table class="table panel-info table-bordered table-hover" id="dataTable">
-                    <thead>
-                    <tr>
-                        <th>ФИО</th>
-                        <th>Возраст</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <g:each in="${phonebookList}">
-                        <tr>
-                            <td><a href="/phonebook/show/${it.id}">${it.surname} ${it.name} ${it.patronymic}</a></td>
-                            <g:set var="birth" value="${it.birthday}"/>
-                            <% duration = new java.util.Date().toTimestamp().minus(birth) %>
-                            <% years = Math.round(duration / 365) %>
-                            <td><a href="/phonebook/show/${it.id}"><%=years%></a></td>
-                        </tr>
-                    </g:each>
-                    </tbody>
-                </table>
-            </div>
-    </g:form>
-            <div class="panel-info col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <label class="h3">Импортировать из CSV</label>
-                    <div class="panel-heading">
-                        <g:uploadForm action="upload">
-                            <input class="panel-info" type="file" name="filecsv"/>
-                            <input class="btn-info" type="submit"/>
-                        </g:uploadForm>
-                    </div>
-            </div>
+        <table class="table panel-info table-bordered table-hover" id="dataTable">
+            <thead>
+            <tr>
+                <th>ФИО</th>
+                <th>Возраст</th>
+            </tr>
+            </thead>
+            <tbody>
+            <g:each in="${phonebookList}">
+                <tr>
+                    <td><a href="/phonebook/show/${it.id}">${it.surname} ${it.name} ${it.patronymic}</a></td>
+                    <g:set var="birth" value="${it.birthday}"/>
+                    <% duration = new java.util.Date().toTimestamp().minus(birth) %>
+                    <% years = Math.round(duration / 365) %>
+                    <td><a href="/phonebook/show/${it.id}"><%=years%></a></td>
+                </tr>
+            </g:each>
+            </tbody>
+        </table>
         </div>
-
-
-
+    </g:form>
+    <div class="panel-info col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <label class="h3">Импортировать из CSV</label>
+        <div class="panel-heading">
+            <g:uploadForm action="upload">
+                <input class="panel-info" type="file" name="filecsv"/>
+                <input class="btn-info" type="submit"/>
+            </g:uploadForm>
+        </div>
+    </div>
     <div class="pagination">
         <g:paginate total="${phonebookCount ?: 0}"/>
     </div>
 </div>
-
-
 
 </body>
 </html>
